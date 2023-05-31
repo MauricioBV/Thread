387e64a4bed6ea3cf3c59ed4228570a0bc650038
MauricioBV <124370784+MauricioBV@users.noreply.github.com>
2023-05-31 08:16:39 -0600

    Add files via upload

---
+++ TestTh.java
@@ -0,0 +1,22 @@
+package com.pp2;
+
+public class TestTh extends Thread {
+	private String nombre;
+	private int retardo;
+	
+	public TestTh(String s, int d) {
+		nombre = s;
+		retardo = d;
+	}
+	public void run() {
+		try {
+			sleep(retardo);
+		}catch(InterruptedException e) {
+			System.out.println("La ejecucion a terminado");
+		}
+		System.out.println("Hola mundo! "+nombre+" "+retardo);
+	
+	}
+	
+	}
+
